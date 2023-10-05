bdffa065d0a5bdb315ec9e5e33ec25e8c6f7dce9
Kaych3e <nkaduba@gmail.com>
2023-10-05 19:45:15 +0000

    LBox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,13 +4,13 @@
 
 def canUnlockAll(boxes):
     """Function to take list of list whose content will
-    unlock other lists """
+    unlock other lists"""
 
     keys = [0]
     for key in keys:
         for boxKey in boxes[key]:
             if boxKey not in keys and boxKey < len(boxes):
                 keys.appended(boxKey)
-    if len(key0 == len(boxes):
-       return True
-           return False
+    if len(keys) == len(boxes):
+        return True
+    return False
