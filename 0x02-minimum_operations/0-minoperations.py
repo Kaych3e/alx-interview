ff2de0fed9e4edd45b19fb90dee291874a7ebf80
Kaych3e <nkaduba@gmail.com>
2023-10-15 21:08:50 +0000

    Min Ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Method that determines the number of minmum operations given n characters
+"""
+
+
+def minOperations(n):
+    """ 
+        Calculates the fewest number of operations (copy, paste)
+        needed to result in exactly n H characters
+    """
+    if n <= 1:
+        return 0
+
+    len_H = 1
+    len_cop = 0
+    all_ops = 0
+
+    while len_H < n:
+        # if len_H is a factor of n, copy and paste
+        if n % len_H == 0:
+            all_ops += 2
+            # save the number of characters in the clipboard
+            len_cop = len_H
+            len_H *= 2
+        else:
+            all_ops += 1
+            len_H += len_cop
+    return all_ops
