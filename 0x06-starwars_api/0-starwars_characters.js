00cb8ca45336cd6a2ddd6140f97bc7fec167643f
Kaych3e <nkaduba@gmail.com>
2023-11-07 05:06:10 -0800

    SW

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+
+const requests = require('request');
+
+function printCharacter(movieId) {
+    // Get movie data from SWAPI
+    request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+
+            // Get character URLs list
+            const characterUrls = data.characters;
+
+            // Fetch each character's name and print
+            characterUrls.forEach(url => {
+                request(url, (error, response, body) => {
+                    if (!error && response.statusCode == 200) {
+                        const data = JSON.parse(body);
+                        console.log(data.name);
+                    }
+                });
+            });
+        }
+    });
+}
+
+// Get the movie ID from the command-line arguments
+const movieId = process.argv[2];
+
+// Print the character names
+printCharacter(movieId);
