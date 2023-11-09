22f781ddbacfd823c8451e3366ee6ce642ff0747
Kaych3e <nkaduba@gmail.com>
2023-11-09 13:19:54 -0800

    API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,22 @@
 #!/usr/bin/node
 /* Script to fetch data from starwars API */
 
-
 const request = require('request');
 const movieUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-
-function printCharacter(movieId) {
+function printCharacter (movieId) {
   // Get movie data from SWAPI
   return new Promise((resolve, reject) => {
     request(movieId, (error, response, body) => {
       if (!error && response.statusCode === 200) {
         const data = JSON.parse(body);
-	resolve(data.name);
-	}
-      });
+        resolve(data.name);
+      }
     });
-  }
+  });
+}
 
-
-function fetchAndPrintCharNames(movieUrls) {
+function fetchAndPrintCharNames (movieUrls) {
   // Fetch each character's name and print
   request(movieUrls, (error, response, body) => {
     if (!error && response.statusCode === 200) {
@@ -28,12 +25,12 @@ function fetchAndPrintCharNames(movieUrls) {
       const characterUrls = CharData.characters;
 
       const promises = characterUrls.map(printCharacter);
-        
+
       Promise.all(promises)
         .then(charNames => {
           charNames.forEach(name => console.log(name));
         });
-      }
+    }
   });
 }
 
