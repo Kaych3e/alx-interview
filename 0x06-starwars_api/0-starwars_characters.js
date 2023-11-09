2ba2e611828e6a3c77aac6ab1e1408ee7e33e0c6
Kaych3e <nkaduba@gmail.com>
2023-11-09 13:04:22 -0800

    API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,40 @@
 #!/usr/bin/node
 
 
-const requests = require('request');
+const request = require('request');
 const movieUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
+
 function printCharacter(movieId) {
-    // Get movie data from SWAPI
+  // Get movie data from SWAPI
+  return new Promise((resolve, reject) => {
     request(movieId, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            const data = JSON.parse(body);
-
-            // Get character URLs list
-            const characterUrls = data.characters;
-
-            // Fetch each character's name and print
-            characterUrls.forEach(url => {
-                request(url, (error, response, body) => {
-                    if (!error && response.statusCode == 200) {
-                        const data = JSON.parse(body);
-                        console.log(data.name);
-                    }
-                });
-            });
-        }
+      if (!error && response.statusCode == 200) {
+        const data = JSON.parse(body);
+	resolve(data.name);
+	}
+      });
     });
+  }
+
+
+function fetchAndPrintCharNames(movieUrls) {
+  // Fetch each character's name and print
+  request(movieUrls, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const CharData = JSON.parse(body);
+      // Get character URLs list
+      const characterUrls = CharData.characters;
+
+      const promises = characterUrls.map(printCharacter);
+        
+      Promise.all(promises)
+        .then(charNames => {
+          charNames.forEach(name => console.log(name));
+        });
+      }
+  });
 }
 
-// Get the movie ID from the command-line arguments
-const movieId = process.argv[2];
-
 // Print the character names
-printCharacter(movieId);
+fetchAndPrintCharNames(movieUrl);
