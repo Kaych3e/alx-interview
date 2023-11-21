771421d8da7cd9637ad2656a2f84af6d2b55b471
Kaych3e <nkaduba@gmail.com>
2023-11-21 00:48:46 -0800

    MC

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,16 +11,16 @@ def makeChange(coins, total):
         return 0
 
     # Initialize an array mc to store minimum number of coins for each amount
-    # Initialize with a value greater than any possible solution
-    mc = [float('inf')] * (total + 1)
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        amount = 0
 
-    # Base case: 0 coins needed for total of 0
-    mc[0] = 0
-
-    # Update the array mc for each coin denomination
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            mc[amount] = min(mc[amount], mc[amount - coin] + 1)
-
-    # If dp[total] is still the initial value, it means total cannot be met
-    return mc[total] if mc[total] != float('inf') else -1
+        # Update the array mc for each coin denomination
+        for mc in coin:
+            while(total >= mc):
+                amount += 1
+                total -= mc
+        if total == 0:
+            return amount
+        return -1
