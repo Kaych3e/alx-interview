53876a300dd5e86fbe866244a10e921326c47d5b
Kaych3e <nkaduba@gmail.com>
2023-11-30 13:48:19 -0800

    IPeri

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """Island perimeter function"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # Count all four sides
+                perimeter += 4
+                # Check neighbors and subtract for each adjacent land
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
